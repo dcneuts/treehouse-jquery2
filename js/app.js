bcaa6a551c30a9975b7fe61e1a786aedcd001581
Derek Neuts <dcneuts@gmail.com>
2017-03-13 23:30:23 -0700

    Finished the form with jQuery, created named functions and used those instead of anonymous functions. Used multiple handlers to confirm the password was indeed a match and hid the warnings when optimal conditions were met.  Variables were loaded in the front to help with named functions, as well as focus and keyup events.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
 // Problem: Hints are still showing
 // Solution: Hide and show at the right time
-
 // Hide the hints based on character length and validation
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
+
 $("form span").hide();
 
 function passwordEvent(){
-    if ($(this).val().length > 8) {
-        $(this).next().hide();
+    if ($password.val().length > 8) {
+        $password.next().hide();
     } else {
-        $(this).next().show();
+        $password.next().show();
+    }
+}
+
+function confirmPasswordEvent(){
+    // Check for password match in both boxes
+    // Hide hint if matched
+    // Show hint if not matched
+    if ($password.val() === $confirmPassword.val()){
+        $confirmPassword.next().hide();
+    } else {
+        $confirmPassword.next().show();
     }
 }
 
 // Validate password characters
-$("#password").focus(passwordEvent).keyup(passwordEvent);
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
 
 // Confirmation box
-
-// Check for password match in both boxes
-
-// Hide hint if matched
-
-// Show hint if not matched
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
