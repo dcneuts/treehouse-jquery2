1de6597ad8aa53abd91d0afd6c3d3ab0037a5feb
Derek Neuts <dcneuts@gmail.com>
2017-03-13 22:35:50 -0700

    Setup JS file for lesson to determine problem, solution and todo lists.

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Problem: Hints are still showing
+// Solution: Hide and show at the right time
+
+// Hide the hints based on character length and validation
+
+    // Validate password characters
+
+    // Hide if valid
+
+    // Else show the hint for password
+
+// Confirmation box
+
+    // Check for password match in both boxes
+
+        // Hide hint if matched
+
+        // Show hint if not matched
